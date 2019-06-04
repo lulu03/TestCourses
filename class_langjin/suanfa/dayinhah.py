d7e96177a56600ef81e3da879d4b829bb513ec64
lulu03 <273134228@qq.com>
2019-06-04 12:53:15 +0800

    test

---
+++ class_langjin/suanfa/dayinhah.py
@@ -0,0 +1,10 @@
+def printhaha():
+    print("哈哈哈哈哈哈哈")
+
+
+def printhaha():
+    print("嘻嘻嘻嘻嘻嘻嘻嘻嘻")
+
+
+def printhaha():
+    print("呵呵呵呵呵呵呵呵呵")
---
+++ class_langjin/suanfa/jjcc.py
@@ -0,0 +1,25 @@
+def jia(a,b):
+    '''
+    这是一个加法
+    '''
+    print(a+b)
+
+def jian(a,b):
+    '''
+    这是一个减法
+    '''
+    print(a-b)
+
+
+def cheng(a,b):
+    '''
+    这是一个乘法
+    '''
+    print(a*b)
+
+
+def chu(a,b):
+    '''
+    这是一个除法
+    '''
+    print(a/b)
